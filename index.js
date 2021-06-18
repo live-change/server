98337cbb223e810abec0ff24f7e0afb31eac5ffe
Michał Łaszczewski <michal@laszczewski.com>
2021-06-18 14:45:29 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const Renderer = require('./lib/Renderer.js')
+const Services = require('./lib/Services.js')
+const SsrServer = require('./lib/SsrServer.js')
+const TestServer = require('./lib/TestServer.js')
+
+const createLoopbackDao = require('./lib/createLoopbackDao.js')
+const renderTemplate = require('./lib/renderTemplate.js')
+const setupApiServer = require('./lib/setupApiServer.js')
+const setupApiSockJs = require('./lib/setupApiSockJs.js')
+const setupApiWs = require('./lib/setupApiWs.js')
+const setupDbServer = require('./lib/setupDbServer.js')
+
+
+module.exports = {
+
+  Renderer,
+  Services,
+  SsrServer,
+  TestServer,
+  
+  createLoopbackDao,
+  renderTemplate,
+  setupApiServer,
+  setupApiSockJs,
+  setupApiWs,
+  setupDbServer
+
+}
